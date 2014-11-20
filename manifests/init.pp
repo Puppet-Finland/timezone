90a24eb8178a66ca318579d707e83fa5b20a1b26
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:34:54 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,13 +16,13 @@
 # == Authors
 # 
 # Samuli Seppänen <samuli.seppanen@gmail.com>
+#
 # Samuli Seppänen <samuli@openvpn.net>  
-# 
+#
 # == License
 #
-# BSD-lisence
-# See file LICENSE for details
-#
+# BSD-license. See file LICENSE for details.
+# 
 class timezone
 (
     $timezone='Etc/UTC'
